b3b704538976213f5ae11c07e06863149cae54ab
David <sanchez.dav90@gmail.com>
2014-07-04 22:18:45 -0500

    Formularios
    Se agrego el formulario de Registro y de login

--- sfotipy/sfotipy/urls.py
+++ sfotipy/sfotipy/urls.py
@@ -6,6 +6,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^tracks/(?P<title>[\w\- ]+)/', 'tracks.views.track_view', name='track_view'),
+    url(r'^signup/', 'userprofiles.views.signup', name='signup'),
+    url(r'^signin/', 'userprofiles.views.signin', name='signin'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
---
+++ sfotipy/userprofiles/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+class UserCreationEmailForm(UserCreationForm):
+	email = forms.EmailField()
+
+	class Meta:
+		model = User
+		fields = ('username', 'email')
+
+	def clean_email(self):
+		email = self.cleaned_data["email"]
+		try:
+			User._default_manager.get(email=email)
+		except User.DoesNotExist:
+			return email
+		raise forms.ValidationError('Usuario duplicado')
+
+
+class EmailAuthenticationForm(forms.Form):
+	email = forms.EmailField()
+	password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+	def __init__(self, *args, **kwargs):
+		self.user_cache = None
+		super(EmailAuthenticationForm, self).__init__(*args, **kwargs)
+
+	def clean(self):
+		email = self.cleaned_data.get('email')
+		password = self.cleaned_data.get('password')
+
+		self.user_cache = authenticate(email=email, password=password)
+
+		if self.user_cache is None:
+			raise forms.ValidationError('Usuario Incorrecto')
+		elif not self.user_cache.is_active:
+			raise forms.ValidationError('Usuario Inactivo')
+
+		return self.cleaned_data
+
+	def get_user(self):
+		return self.user_cache
